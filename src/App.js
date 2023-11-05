3b654473f8d053e8a06e72a5a1b657ce99e3c019
coding8210018454 <mdshahidkatihar002582@gmail.com>
2023-11-05 07:04:45 +0530

    my first react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
+
+
 function App() {
+   const [total, setTotal] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="container">
+        <div className="calculator">
+            <div className="input-field">
+              <input className='inp'  type="text" value={total}/>
+            </div>
+            <div>
+               <input className='btn' type="button" value="C" onClick={()=> setTotal("")}/>
+               <input className='btn' type="button" value="DE" onClick={(e)=>setTotal(total.toString().slice(0, -1))}/>
+               <input className='btn' type="button" value="%" onClick={(e)=> setTotal(total + e.target.value)}/>
+               <input className='btn' type="button" value="/" onClick={(e)=> setTotal(total + e.target.value)} />
+            </div>
+
+            <div>
+               <input className='btn' type="button" value="7" onClick={(e)=> setTotal(total + e.target.value)} />
+               <input className='btn' type="button" value="8" onClick={(e)=> setTotal(total + e.target.value)}/>
+               <input className='btn' type="button" value="9" onClick={(e)=> setTotal(total + e.target.value)}/>
+               <input className='btn clr' type="button" value="*" onClick={(e)=> setTotal(total + e.target.value)}/>
+            </div>
+
+            <div>
+               <input className='btn' type="button" value="4" onClick={(e)=> setTotal(total + e.target.value)}/>
+               <input className='btn' type="button" value="5" onClick={(e)=> setTotal(total + e.target.value)}/>
+               <input className='btn' type="button" value="6" onClick={(e)=> setTotal(total + e.target.value)}/>
+               <input className='btn clr' type="button" value="-" onClick={(e)=> setTotal(total + e.target.value)}/>
+            </div>
+
+            <div>
+               <input className='btn' type="button" value="1" onClick={(e)=> setTotal(total + e.target.value)} />
+               <input className='btn' type="button" value="2" onClick={(e)=> setTotal(total + e.target.value)} />
+               <input className='btn' type="button" value="3" onClick={(e)=> setTotal(total + e.target.value)} />
+               <input className='btn clr' type="button" value="+" onClick={(e)=> setTotal(total + e.target.value)} />
+            </div>
+
+            <input className='btn zero' type="button" value="0" onClick={(e)=> setTotal(total + e.target.value)} />
+               <input className='btn' type="button" value="." onClick={(e)=> setTotal(total + e.target.value)} />
+               <input className='btn clr' type="button" value="=" onClick={()=>setTotal(eval(total))} />
+        </div>
+      </div>
+    </>
   );
 }
 
